5b0658ff589722ec54faad231cf02ee3a1adeaf8
anastasiia06 <anastasiia.lily06@gmail.com>
2025-09-02 12:18:03 +0300

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form3
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Location = new Point(104, 222);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 21);
+            label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(104, 149);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(154, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(255, 192, 255);
+            button1.Font = new Font("Segoe UI", 12F);
+            button1.Location = new Point(127, 347);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 44);
+            button1.TabIndex = 2;
+            button1.Text = "Розрахунок";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(362, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form3";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Channels;
+
+namespace WinFormsApp4
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string[] a = s.Split(" ");
+            int maxLength = 0;
+            foreach (string word in a)
+            {
+                if (word.Length > maxLength)
+                    maxLength = word.Length;
+            }
+            string result = "";
+            for (int len = 1; len <= maxLength; len++)
+            {
+                foreach (string word in a)
+                {
+                    if (word.Length == len)
+                    {
+                        if (result != "")
+                            result += " "; 
+                        result += word;
+                    }
+                }
+            }
+            label1.Text = $"Результат: {result}";
+
+        }
+    }
+}
